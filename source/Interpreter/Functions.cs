9fb44a51d39a5aa17696b6c812cc0c31f8185353
Anmol Sinha <anmolsinha1201@outlook.com>
2018-03-08 18:01:29 +0530

    Added basic parameter support for functions

--- source/Interpreter/Functions.cs
+++ source/Interpreter/Functions.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using static AHKCore.Nodes;
 using static AHKCore.Query;
 
@@ -11,6 +13,18 @@ namespace AHKCore
 			var function = indexed.Functions[context.functionName][0];
 			var oIndexed = indexed;
 			indexed = new IndexedNode();
+
+			var parameterVariableAssignList = new List<variableAssignClass>();
+			for(int i = 0; i < context.functionParameterList.Count; i++)
+			{
+				parameterVariableAssignList.Add(
+					new variableAssignClass(
+						new complexVariableClass(null, new List<BaseAHKNode>() {function.functionHead.functionParameters[i].variableName}),
+					"=", (BaseAHKNode)context.functionParameterList[i])
+				);
+			}
+
+			function.functionBody = parameterVariableAssignList.Concat(function.functionBody).ToList();
 
 			foreach (var functionNode in function.functionBody)
 				traverser.objectDispatcher(functionNode);
