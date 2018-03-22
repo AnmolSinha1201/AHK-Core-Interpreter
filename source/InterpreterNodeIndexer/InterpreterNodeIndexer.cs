8e725d24e737ef633ac96ec899f4711a4d5076d2
Anmol Sinha <anmolsinha1201@outlook.com>
2018-03-22 12:44:41 +0530

    Added assembly includes

---
+++ source/InterpreterNodeIndexer/InterpreterNodeIndexer.cs
@@ -0,0 +1,35 @@
+using static AHKCore.Nodes;
+using System;
+using System.Runtime.Loader;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+
+namespace AHKCore
+{
+	class InterpreterNodeIndexer: NodeIndexer
+	{
+		public InterpreterAssemblyMapping assemblyMap;
+
+		public InterpreterNodeIndexer(InterpreterAssemblyMapping assemblyMap)
+		{
+			this.assemblyMap = assemblyMap;
+		}
+
+		public override BaseAHKNode othersFilter(BaseAHKNode context)
+		{
+			if (context is directiveClass d && d.directiveName.ToLower() == "include")
+			{
+				LoadAssembly(d.directiveParam);
+			}
+			return context;
+		}
+
+		void LoadAssembly(string fileName)
+		{
+			var assembly =  AssemblyLoadContext.Default.LoadFromAssemblyPath(
+				Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), fileName));
+			assemblyMap.mapNamespace(assembly);
+		}
+	}
+}
