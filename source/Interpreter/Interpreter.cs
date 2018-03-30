36ce1045d0f297441d3e6dfcd200cc21ee54d4b6
Anmol Sinha <anmolsinha1201@outlook.com>
2018-03-30 15:34:28 +0530

    Converted project to loadable assembly

--- source/Interpreter/Interpreter.cs
+++ source/Interpreter/Interpreter.cs
@@ -3,12 +3,13 @@ using System;
 
 namespace AHKCore
 {
-	class Interpreter
+	public class Interpreter
 	{
-		public void Interpret()
+		public IndexedNode Interpret(string text)
 		{
 			var parserInstance = new Parser();
-			var AHKNodes = parserInstance.parse("#include, Stub.dll\nFunctions.Test()\n#using, Functions\nTest()\nvar=zxc()\nzxc(){return 1}\nzxc.qwe=123\nzxc.asd(123)\nclass zxc{asd(varA){varZ=99}}\nasd(123, 456)\nasd(varA, varB=123, varC=789){var1=123\nvar2=456\nvar1=var2}");
+			// var AHKNodes = parserInstance.parse("#include, Stub.dll\nFunctions.Inner.Test2()\n#using, Functions\nInner.Test2()\nvar=zxc()\nzxc(){return 1}\nzxc.qwe=123\nzxc.asd(123)\nclass zxc{asd(varA){varZ=99}}\nasd(123, 456)\nasd(varA, varB=123, varC=789){var1=123\nvar2=456\nvar1=var2}");			
+			var AHKNodes = parserInstance.parse(text);
 			
 			var assemblyMap = new InterpreterAssemblyMapping();
 
@@ -25,6 +26,8 @@ namespace AHKCore
 			// manually calling objectDispatcher which will call its visitor functions
 			foreach (var o in indexedNodes.AutoExecute)
 				traverser.objectDispatcher(o);
+
+			return indexedNodes;
 		}
 	}
 }
--- source/Program.cs
+++ source/Program.cs
@@ -7,7 +7,7 @@ namespace AHKCore
         static void Main(string[] args)
         {
             var interpreter = new Interpreter();
-            interpreter.Interpret();
+            interpreter.Interpret("#include, Stub.dll\nFunctions.Inner.Test2()\n#using, Functions\nInner.Test2()\nvar=zxc()\nzxc(){return 1}\nzxc.qwe=123\nzxc.asd(123)\nclass zxc{asd(varA){varZ=99}}\nasd(123, 456)\nasd(varA, varB=123, varC=789){var1=123\nvar2=456\nvar1=var2}");
         }
     }
 }
