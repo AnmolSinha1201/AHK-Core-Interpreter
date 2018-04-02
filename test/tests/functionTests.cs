4def053e1402b644d21340859b54704fa63cfff0
Anmol Sinha <anmolsinha1201@outlook.com>
2018-04-02 12:44:58 +0530

    Class based function call tests added

--- test/tests/functionTests.cs
+++ test/tests/functionTests.cs
@@ -44,5 +44,45 @@ namespace test
 				return retVal.AsFalse();
 			return retVal.AsTrue();
 		}
+
+		static TestReturnClass classFunctionCall()
+		{
+			var retVal = new TestReturnClass("class function call");
+			var indexed = interpreter.Interpret("var=class.function()\nclass class{function(){return 123}}");
+
+			if (indexed.Variables["var"].Value.ToString() != "123")
+				return retVal.AsFalse();
+			return retVal.AsTrue();
+		}
+
+		static TestReturnClass classFunctionCall2()
+		{
+			var retVal = new TestReturnClass("class function call variable passing (no default)");
+			var indexed = interpreter.Interpret("var=class.function(123)\nclass class{function(var1){return var1}}");
+
+			if (indexed.Variables["var"].Value.ToString() != "123")
+				return retVal.AsFalse();
+			return retVal.AsTrue();
+		}
+
+		static TestReturnClass classFunctionCall3()
+		{
+			var retVal = new TestReturnClass("class function call variable passing (default value)");
+			var indexed = interpreter.Interpret("var=class.function(123)\nclass class{function(var1=456){return var1}}");
+
+			if (indexed.Variables["var"].Value.ToString() != "123")
+				return retVal.AsFalse();
+			return retVal.AsTrue();
+		}
+
+		static TestReturnClass classFunctionCall4()
+		{
+			var retVal = new TestReturnClass("class function call variable passing (default value, no value passed)");
+			var indexed = interpreter.Interpret("var=class.function()\nclass class{function(var1=456){return var1}}");
+
+			if (indexed.Variables["var"].Value.ToString() != "456")
+				return retVal.AsFalse();
+			return retVal.AsTrue();
+		}
 	}
 }
