46d16e8ac461c22cb8c0079e5c217f2ef9f8ae68
Anmol Sinha <anmolsinha1201@outlook.com>
2018-04-23 12:06:34 +0530

    Revert "Added assembly type instance function call"
    This reverts commit 4199f03d09ca9912f319bf104d9713f818775075.

--- source/InterpreterVisitor/Scoping.cs
+++ source/InterpreterVisitor/Scoping.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System;
 using System.Linq;
 using System.Reflection;
-using static AHKCore.IndexedNodesFragment.Variables;
 
 namespace AHKCore
 {
@@ -15,14 +14,8 @@ namespace AHKCore
 			{
 				if (context.chain[i] is variableClass v)
 				{
-					if (indexed.Variables.Exists(v.variableName))
-					{
-						variable(v);
-						if (((VariableValue)v.extraInfo).Value is IndexedNode ind)
-							indexed = ind;
-						//TODO : fix checking types again in assembly scoping
-						else return assemblyScope(context, i);
-					}
+					if (indexed.Variables.Exists(v.variableName) && v.extraInfo is IndexedNode ind)
+						indexed = ind;
 					else if (indexed.Classes[v.variableName] != null)
 					{
 						indexed = indexed.Classes[v.variableName];
