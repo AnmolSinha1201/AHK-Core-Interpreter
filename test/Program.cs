e7cf156ee9520ef5468c244d243f6c11d0801109
Anmol Sinha <anmolsinha1201@outlook.com>
2018-06-27 15:33:47 +0530

    Added binaryOP tests

--- test/Program.cs
+++ test/Program.cs
@@ -8,7 +8,7 @@ namespace test
 	{
 		static void Main(string[] args)
 		{
-			Test.TestFunction(TestCases.binaryOpTest1);
+			Test.TestFunction(TestCases.binaryOpLogicalTest3);
 			Test.TestAll();
 		}
 	}
--- test/tests/BinaryOpTests.cs
+++ test/tests/BinaryOpTests.cs
@@ -7,11 +7,65 @@ namespace test
 		public static TestResult binaryOpTest1()
 		{
 			var retVal = TestRunner.Test("var=1+2+3*4-5-6");
-			// var retVal = TestRunner.Test("var=1*(2+3)*4");
 			if (retVal.result == TestResult.Exception)
 				return TestResult.Exception;
 			
 			if (retVal.indexed.Variables["var"].Value.ToString() != "4")
+				return TestResult.Failed;
+			return TestResult.Passed;
+		}
+
+		public static TestResult binaryOpTest2()
+		{
+			var retVal = TestRunner.Test("var=1*(2+3)*4");
+			if (retVal.result == TestResult.Exception)
+				return TestResult.Exception;
+			
+			if (retVal.indexed.Variables["var"].Value.ToString() != "20")
+				return TestResult.Failed;
+			return TestResult.Passed;
+		}
+
+		public static TestResult binaryOpLogicalTest1()
+		{
+			var retVal = TestRunner.Test("var=1=1");
+			if (retVal.result == TestResult.Exception)
+				return TestResult.Exception;
+			
+			if (retVal.indexed.Variables["var"].Value.ToString() != "True")
+				return TestResult.Failed;
+			return TestResult.Passed;
+		}
+
+		public static TestResult binaryOpLogicalTest2()
+		{
+			var retVal = TestRunner.Test("var1=\"QWE\"\nvar2=\"qwe\"\nvar=var1=var2");
+			if (retVal.result == TestResult.Exception)
+				return TestResult.Exception;
+			
+			if (retVal.indexed.Variables["var"].Value.ToString() != "True")
+				return TestResult.Failed;
+			return TestResult.Passed;
+		}
+
+		public static TestResult binaryOpLogicalTest3()
+		{
+			var retVal = TestRunner.Test("var1=\"asd\"\nvar2=\"qwe\"\nvar=var1=var2");
+			if (retVal.result == TestResult.Exception)
+				return TestResult.Exception;
+			
+			if (retVal.indexed.Variables["var"].Value.ToString() != "False")
+				return TestResult.Failed;
+			return TestResult.Passed;
+		}
+
+		public static TestResult binaryOpLogicalTest4()
+		{
+			var retVal = TestRunner.Test("var1=123\nvar=var1=123");
+			if (retVal.result == TestResult.Exception)
+				return TestResult.Exception;
+			
+			if (retVal.indexed.Variables["var"].Value.ToString() != "True")
 				return TestResult.Failed;
 			return TestResult.Passed;
 		}
